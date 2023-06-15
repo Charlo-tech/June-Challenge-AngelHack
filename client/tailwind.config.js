07f3b570b919cb4ebf425af7285072396a90393b
Charlo-tech <charliewachira124@gmail.com>
2023-06-15 15:57:36 +0300

    final edit

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  content: ["./src/**/*.{html,js}"],
+  // purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
